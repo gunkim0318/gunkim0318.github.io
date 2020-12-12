a195ee5dc46f379cf8502312f6c3d72d940e4a12
gunkim <gunkim0318@gmail.com>
2020-12-12 09:44:20 +0900

    카테고리 불러오는 기능 쿼리 스트링 방식으로 작동하도록 수정.

--- js/PagingUtil.js
+++ js/PagingUtil.js
@@ -8,6 +8,13 @@ var PagingUtil = function(pageNum, totalPostsNum){
     if(totalPostsNum%this.viewPostsNum != 0){
         this.totalPageNum += 1;
     }
+    if(this.totalPageNum < this.pageNum){
+        this.pageNum = this.totalPageNum;
+    }
+    if(this.pageNum < 1){
+        this.pageNum = 1;
+    }
+
     this.isPrev = pageNum != 1;
     this.isNext = pageNum != this.totalPageNum;
 }
