e92b6fbe7607d549de8a39e1d25530e342ecef3e
kanpann <gunkim.dev@gmail.com>
2021-03-11 00:31:43 +0900

    URL 형식 수정

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { TARGET_CLASS } from '../../utils/visible'
+import React from "react";
+import { Link } from "gatsby";
+import { TARGET_CLASS } from "../../utils/visible";
 
-import './index.scss'
+import "./index.scss";
 
 export const ThumbnailItem = ({ node }) => (
-  <Link className={`thumbnail ${TARGET_CLASS}`} to={node.frontmatter.title}>
+  <Link
+    className={`thumbnail ${TARGET_CLASS}`}
+    to={node.frontmatter.title.substr(17)}
+  >
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
-)
+);
