8ff8668a69c49933495ef1d12748abdf44369300
kanpann <gunkim.dev@gmail.com>
2021-03-10 22:30:59 +0900

    Gatsby 테마로 변경

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: `Gunlog`,
+  description: `Blog posted about ...`,
+  author: `Gun Kim`,
+  introduction: `안녕하세요. 공부하거나 알고 있는 내용들을 정제해보기 위해 블로그를 시작했습니다.`,
+  siteUrl: `https://gunlog.dev`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: ``, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jbee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+}
