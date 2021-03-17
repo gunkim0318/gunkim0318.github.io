2d59ea457c36c524f9ac23ac24efd1d2c0208cea
kanpann <gunkim.dev@gmail.com>
2021-03-17 09:34:34 +0900

    이전, 다음 포스트 클릭 시 404 에러 수정

--- src/components/post-navigator/index.jsx
+++ src/components/post-navigator/index.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 
-import './index.scss'
+import "./index.scss";
 
 export const PostNavigator = ({ pageContext }) => {
-  const { previous, next } = pageContext
+  const { previous, next } = pageContext;
 
   return (
     <ul className="navigator">
       <li>
         {previous && (
-          <Link to={previous.fields.slug} rel="prev">
+          <Link to={"/" + previous.fields.slug.substr(17)} rel="prev">
             ← {previous.frontmatter.title}
           </Link>
         )}
       </li>
       <li>
         {next && (
-          <Link to={next.fields.slug} rel="next">
+          <Link to={"/" + next.fields.slug.substr(17)} rel="next">
             {next.frontmatter.title} →
           </Link>
         )}
       </li>
     </ul>
-  )
-}
+  );
+};
