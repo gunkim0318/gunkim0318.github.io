607bd3ae05d1aea428f52cdd6130dc22ae87d083
kanpann <gunkim.dev@gmail.com>
2021-03-17 10:01:47 +0900

    게시글 목록 썸네일 이미지 표출하도록 수정

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import { Link } from "gatsby";
 import { TARGET_CLASS } from "../../utils/visible";
-
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import "./index.scss";
 
-export const ThumbnailItem = ({ node }) => (
-  <Link
-    className={`thumbnail ${TARGET_CLASS}`}
-    to={node.fields.slug.substr(17)}
-  >
-    <div key={node.fields.slug}>
-      <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-    </div>
-  </Link>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export const ThumbnailItem = ({ node }) => {
+  const classes = useStyles();
+  return (
+    <Link
+      className={`thumbnail ${TARGET_CLASS}`}
+      to={node.fields.slug.substr(17)}
+    >
+      <div key={node.fields.slug}>
+        <Grid container spacing={3}>
+          <Grid item md={4} xs={12}>
+            <img src={node.frontmatter.image} />
+          </Grid>
+          <Grid item md={8} xs={12}>
+            <h3>{node.frontmatter.title || node.fields.slug}</h3>
+            <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </Grid>
+        </Grid>
+      </div>
+    </Link>
+  );
+};
